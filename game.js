88b649bc66e0ecb4fef3e9b92f5c2d0c8dab0346
whyrename <qweqewq@example.com>
2024-12-04 14:07:11 +0200

    16.Downloading and playing back sounds

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
-const KEYS = {
+const KEYS = { 
           LEFT: 37,
           RIGHT: 39,
           SPACE: 32
-      };
+};
       
       let game = {
           running: true,
           ctx: null,
           platform: null,
           ball: null,
+          score: 0,
           blocks: [],
           rows: 4,
           cols: 8,
@@ -20,7 +21,10 @@ const KEYS = {
               platform: null,
               block: null
           },
-          init: function () {
+          sounds: {
+                    bump: null
+          },
+          init() {
               this.ctx = document.getElementById("mycanvas").getContext("2d");
               this.setEvents();
           },
@@ -37,18 +41,32 @@ const KEYS = {
               });
           },
           preload(callback) {
-              let loaded = 0;
-              let required = Object.keys(this.sprites).length;
-              let onImageLoad = () => {
-                  ++loaded;
-                  if (loaded >= required) {
-                      callback();
-                  }
-              };
+                      let loaded = 0;
+                      let required = Object.keys(this.sprites).length;
+                      required += Object.keys(this.sounds).length;
+            
+                      let onResourceLoad = () => {
+                      ++loaded;
+                      if (loaded >= required) {
+                                callback();
+                      }
+                      };
+            
+                      this.preloadSprites(onResourceLoad);
+                      this.preloadAudio(onResourceLoad);
+            },
+
+          preloadSprites(onResourceLoad) {
               for (let key in this.sprites) {
                   this.sprites[key] = new Image();
                   this.sprites[key].src = "img/" + key + ".png";
-                  this.sprites[key].addEventListener("load", onImageLoad);
+                  this.sprites[key].addEventListener("load", onResourceLoad);
+              }
+          },
+          preloadAudio(onResourceLoad) {
+              for (let key in this.sounds) {
+                  this.sounds[key] = new Audio("sounds/" + key + ".mp3");
+                  this.sounds[key].addEventListener("canplaythrough", onResourceLoad, { once: true });
               }
           },
           create() {
@@ -72,25 +90,25 @@ const KEYS = {
               this.platform.move();
               this.ball.move();
           },
-
-          addScore(){
-                    ++this.score;
-                    if (this.score >= this.blocks.length) {
-                        this.end("Вы выиграли");
-          }
-
-},
-
+          addScore() {
+              ++this.score;
+              if (this.score >= this.blocks.length) {
+                  this.end("Вы выиграли!");
+              }
+          },
           collideBlocks() {
               for (let block of this.blocks) {
                   if (block.active && this.ball.collide(block)) {
                       this.ball.bumpBlock(block);
+                      this.addScore();
+                    this.sounds.bump.play();
                   }
               }
           },
           collidePlatform() {
               if (this.ball.collide(this.platform)) {
                   this.ball.bumpPlatform(this.platform);
+                  this.sounds.bump.play();
               }
           },
           run() {
@@ -117,12 +135,17 @@ const KEYS = {
                   }
               }
           },
-          start: function () {
+          start: function() {
               this.init();
               this.preload(() => {
                   this.create();
                   this.run();
               });
+          },
+          end(message) {
+              this.running = false;
+              alert(message);
+              window.location.reload();
           },
           random(min, max) {
               return Math.floor(Math.random() * (max - min + 1) + min);
@@ -178,16 +201,18 @@ const KEYS = {
               if (ballLeft < worldLeft) {
                   this.x = 0;
                   this.dx = this.velocity;
+                  game.sounds.bump.play();
               } else if (ballRight > worldRight) {
                   this.x = worldRight - this.width;
                   this.dx = -this.velocity;
+                    game.sounds.bump.play();
               } else if (ballTop < worldTop) {
                   this.y = 0;
                   this.dy = this.velocity;
+                  game.sounds.bump.play();
               } else if (ballBottom > worldBottom) {
-                  game.running = false;
-                  alert("Вы проиграли");
-                  window.location.reload();
+                  game.end("Вы проиграли!");
+                  
               }
           },
           bumpBlock(block) {
@@ -247,10 +272,8 @@ const KEYS = {
           },
           collideWorldBounds() {
               let x = this.x + this.dx;
-      
               let platformLeft = x;
               let platformRight = platformLeft + this.width;
-      
               let worldLeft = 0;
               let worldRight = game.width;
       
